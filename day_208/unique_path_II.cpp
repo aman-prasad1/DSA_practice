c9043a1d6cabd1f7f055337050675b0b481b5e5c
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-03-10 09:50:53 +0000

    day 208

---
+++ day_208/unique_path_II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        if(obstacleGrid[0][0] == 0)
+            dp[1][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i == 1 && j == 1) || obstacleGrid[i-1][j-1] == 1)
+                    continue;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+    }
+};
