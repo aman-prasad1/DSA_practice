162b5a07f80ac1f0b219fd7efed6e4850f529013
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-03-30 18:01:37 +0530

    Update lowest_common_ancestor_of_BST.cpp

--- day_228/lowest_common_ancestor_of_BST.cpp
+++ day_228/lowest_common_ancestor_of_BST.cpp
@@ -3,7 +3,7 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         int small = min(p->val, q->val);
         int large = max(p->val, q->val);
-        while (root != nullptr) {
+        while (root != NULL) {
             if (root->val > large)
                 root = root->left;
             else if (root->val < small)
