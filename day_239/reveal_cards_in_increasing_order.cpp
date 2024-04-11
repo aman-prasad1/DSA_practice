995c9b1571fc36f03221a789267d6d77f9d9d9b0
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-04-11 16:57:04 +0530

    Update reveal_cards_in_increasing_order.cpp

--- day_239/reveal_cards_in_increasing_order.cpp
+++ day_239/reveal_cards_in_increasing_order.cpp
@@ -1,1 +1,23 @@
-hello
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(), deck.end());
+
+        queue<int> q;
+        for(int i=0; i<deck.size(); i++){
+            q.push(i);
+        }
+        vector<int> ans(deck.size());
+        for(int i : deck){
+            int front = q.front();
+            q.pop();
+            ans[front] = i;
+            if(!q.empty()){
+                front = q.front();
+                q.pop();
+                q.push(front);
+            }
+        }
+        return ans;
+    }
+};
