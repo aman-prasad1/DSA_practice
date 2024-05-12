7044218c3e16ba34e92bddae828fe103b62afe51
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-05-12 16:27:27 +0530

    Create contains_duplicate_II.cpp

---
+++ day_271/contains_duplicate_II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        unordered_map<int,int> mp;
+
+        for(int i=0; i<n; i++){
+            if(mp.find(nums[i]) != mp.end()){
+                if(abs(mp[nums[i]] - i) <= k) return true;
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
+};
