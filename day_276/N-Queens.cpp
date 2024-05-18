ac9751ccaffab169459cf7848f71ffdaabf8d09d
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-05-18 07:44:51 +0530

    Update N-Queens.cpp

--- day_276/N-Queens.cpp
+++ day_276/N-Queens.cpp
@@ -3,24 +3,23 @@ class Solution {
 
     bool isSafe(int i, int j, int n, vector<string>& board){
         // checking for col j
-        for(int r=0; r<n; r++){
+        for(int r=0; r<i; r++){
             if(board[r][j] == 'Q')
                 return false;
         }
-        // checking for diagonal from top left to bottom right
-        int mini = min(i, j);
-        int r = i-mini, c = j-mini;
-        while(r < n && c < n){
-            if(board[r][c] == 'Q' && r != i && c != j) return false;
-            r++, c++;
+        // checking for left diagonal
+        int r = i-1, c = j-1;
+        while(r >= 0 && c >= 0){
+            if(board[r][c] == 'Q'){
+                return false;
+            }
+            r--, c--;
         }
-        // checking for diagonal from top right to bottom left
-        int temp = i-n+j+1;
-        r = (temp <= 0)? 0:temp;
-        c = (i+j >= n)? n-1 : i+j;
-        while(r < n && c >= 0){
-            if(board[r][c] == 'Q' && r != i && c != j) return false;
-            r++, c--;
+        // checking for right diagonal
+        r = i-1, c = j+1;
+        while(r >= 0 && c < n){
+            if(board[r][c] == 'Q') return false;
+            r--, c++;
         }
         return true;
     }
