9a94abf67b405f643b014cdec4714e8df5593125
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-06-01 16:00:47 +0530

    Create score_of_a_string.cpp

---
+++ day_291/score_of_a_string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans = 0;
+        for(int i=0; i<s.length()-1; i++){
+            ans += abs(s[i]-s[i+1]);
+        }
+        return ans;
+    }
+};
