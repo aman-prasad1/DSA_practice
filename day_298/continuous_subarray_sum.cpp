f0850dac9d867cec95c2539f7256e1a494689f28
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-06-08 22:05:26 +0530

    Create continuous_subarray_sum.cpp

---
+++ day_298/continuous_subarray_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        int sum = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            int rem = sum % k;
+            if(mp.find(rem) == mp.end()) mp[rem] = i;
+            else if(i - mp[rem] > 1) return true;
+        }
+        return false;
+    }
+};
