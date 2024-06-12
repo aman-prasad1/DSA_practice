5af7ab39f3ba9114c70c369df62df9dfc019af72
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-06-12 17:06:41 +0530

    Update construct_quad_tree.cpp

--- day_302/construct_quad_tree.cpp
+++ day_302/construct_quad_tree.cpp
@@ -1,46 +1,70 @@
-class Solution {
+/*
+// Definition for a QuadTree node.
+class Node {
 public:
-    bool sameVal(vector<vector<int>>& grid, int x, int y, int size)
-    {
-        for(auto i = x; i<x+size; ++i)
-        {
-            for(auto j=y; j<y+size; ++j)
-            {
-                if(grid[i][j] != grid[x][y])
-                {
-                    return false;
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
+class Solution {
+    Node* solve(vector<vector<int>>& grid, int r, int c, int n){
+        int same = true;
+        for(int i=r; i<n+r; i++){
+            for(int j=c; j<n+c; j++){
+                if(grid[r][c] != grid[i][j]){
+                    same = false;
+                    break;
                 }
             }
         }
-        return true;
-    }
-    void rec(Node* root, vector<vector<int>>& grid, int x, int y, int size)
-    {
-        if(sameVal(grid, x, y, size))
-        {
-            root->val = grid[x][y];
-            root->isLeaf=true;
-            return;
+        if(same){
+            return new Node(grid[r][c], true);
         }
-        int new_size = size/2;
-        
-        root->topLeft = new Node();
-        rec(root->topLeft, grid, x, y, new_size);
-        
-        root->topRight = new Node();
-        rec(root->topRight, grid, x, y+new_size, new_size);
-        
-        root->bottomLeft = new Node();
-        rec(root->bottomLeft, grid, x+new_size, y, new_size);
 
-        root->bottomRight = new Node();
-        rec(root->bottomRight, grid, x+new_size, y+new_size, new_size);
+        n = n/2;
+
+        Node* topleft = solve(grid, r, c, n);
+        Node* topright = solve(grid, r, c+n, n);
+        Node* bottomleft = solve(grid, r+n, c, n);
+        Node* bottomright = solve(grid, r+n, c+n, n);
+
+        Node* ans = new Node(0, false, topleft, topright, bottomleft, bottomright);
+        return ans;
     }
-    Node* construct(vector<vector<int>>& grid) 
-    {
-        int n = grid.size();
-        auto root = new Node();
-        rec(root, grid, 0, 0, n);
-        return root;
+public:
+    Node* construct(vector<vector<int>>& grid) {
+        return solve(grid, 0, 0, grid.size());
     }
 };
