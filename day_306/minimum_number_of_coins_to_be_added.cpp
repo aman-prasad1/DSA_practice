1f796189db3383d0176cb603c287d03e8021c715
Aman Prasad <110687183+AmanPrasad88@users.noreply.github.com>
2024-06-16 16:13:25 +0530

    Update minimum_number_of_coins_to_be_added.cpp

--- day_306/minimum_number_of_coins_to_be_added.cpp
+++ day_306/minimum_number_of_coins_to_be_added.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int minimumAddedCoins(vector<int>& coins, int target) {
         sort(coins.begin(), coins.end());
-        int size = coins.size();
+
+        int n = coins.size();
         long long sum = 0;
-        int patch = 0;
+        int ans = 0;
         int i = 0;
         while(sum < target){
-            if(i < size && sum+1 >= coins[i]){
+            if(i < n && sum+1 >= coins[i]){
                 sum += coins[i];
                 i++;
             }
             else{
-                patch++;
+                ans++;
                 sum += sum+1;
             }
         }
-        return patch;
+        return ans;
     }
 };
