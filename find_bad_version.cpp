3bafc2d2cce42683d572df6a6fbbc48881cd064f
Aman Prasad <110687183+aman-prasad1@users.noreply.github.com>
2024-08-03 15:33:39 +0530

    Create find_bad_version.cpp

---
+++ find_bad_version.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s = 1, e = n;
+
+        while(s <= e){
+            int mid = s + (e-s)/2;
+
+            if(isBadVersion(mid)) e = mid-1;
+            else s = mid+1;
+        }
+        
+        return s;
+    }
+};
